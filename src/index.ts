0cab12780b06cf06db57420cff48ce188f303e43
Nnamani John <nnamanijohn123@gmail.com>
2022-12-25 00:28:38 +0100

    image proc

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
 import express from 'express';
+import routes from './routes/index';
+import File from './file';
 
-const app = express();
-const port = 3000;
+const app: express.Application = express();
+const port = 3000; // Default port
 
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.send('server working');
+// Add routes
+app.use(routes);
+
+// Start server
+app.listen(port, async (): Promise<void> => {
+  // Make sure that thumb path is available
+  await File.createThumbPath();
+
+  const url = `\x1b[2mhttp://localhost:${port}\x1b[0m`;
+  console.log(`Please open ${url} to review the project ...`);
 });
 
-app.listen(port, () => {
-  console.log(`server started at http://localhost:${port}`);
-});
+export default app;
